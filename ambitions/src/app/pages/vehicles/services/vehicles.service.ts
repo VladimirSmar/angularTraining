b23a512290c6795a2db09f7a1c109eceda1d1c68
L1nett <vladimirshumansky@coherentsolutions.com>
2022-05-24 17:14:49 +0300

    mistakes fix

--- ambitions/src/app/pages/vehicles/services/vehicles.service.ts
+++ ambitions/src/app/pages/vehicles/services/vehicles.service.ts
@@ -1,28 +1,31 @@
 import { Injectable } from '@angular/core';
+import { IVehicle } from 'src/app/interfaces/vehicle';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VehiclesService {
+  private vehicles: IVehicle[] = [
+    {
+      model: 'Yamaha',
+      productionYear: 1985,
+      price: 2000,
+    },
+    {
+      model: 'Yoyota',
+      productionYear: 2003,
+      price: 20200,
+    },
+    {
+      model: 'Korolla',
+      productionYear: 2009,
+      price: 50000,
+    },
+  ];
+
   constructor() {}
 
-  getVehiclesData() {
-    return [
-      {
-        model: 'Yamaha',
-        productionYear: 1985,
-        price: 2000,
-      },
-      {
-        model: 'Yoyota',
-        productionYear: 2003,
-        price: 20200,
-      },
-      {
-        model: 'Korolla',
-        productionYear: 2009,
-        price: 50000,
-      },
-    ];
+  getVehiclesData(): IVehicle[] {
+    return this.vehicles;
   }
 }
